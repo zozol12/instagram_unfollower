efe80d42320d1939746eb5bb0125452567535909
zozol12 <zozolek11022@gmail.com>
2020-11-20 02:01:51 +0100

    edit

--- InstaUnfollower.py
+++ InstaUnfollower.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 
 class InstagramUnfollower:
 
     _username = None
-    _blacklist = []
+    _unfollow_list = []
 
     def __init__(self, username, password, unfollowing_speed):
         self.username = username # the username of your instagram account
@@ -16,10 +17,9 @@ class InstagramUnfollower:
     def login(self): # method to login into your account
         driver = self.driver
         driver.get("https://www.instagram.com/")
-        time.sleep(2)
-        login_button = driver.find_element_by_xpath("//a[@href='/accounts/login/']")
-        login_button.click()
-        time.sleep(2)
+        time.sleep(3)
+        accept_cookies_button = driver.find_element_by_class_name("bIiDR")
+        accept_cookies_button.click()
         username_box = driver.find_element_by_xpath("//input[@name='username']")
         username_box.clear()
         username_box.send_keys(self.username)
@@ -27,94 +27,57 @@ class InstagramUnfollower:
         password_box.clear()
         password_box.send_keys(self.password)
         password_box.send_keys(Keys.RETURN)
-        time.sleep(3)
-        not_now_button =  driver.find_element_by_xpath("//a[@href='/']")
+        time.sleep(5)
+        not_now_button = driver.find_element_by_class_name("yWX7d")
         not_now_button.click()
-        time.sleep(2)
+        time.sleep(3)
 
     def find_username(self): # finds the username in the case of the program user is entered an e-mail adress instead of the username as a login info
         driver = self.driver
         self._username = driver.find_element_by_xpath("//a[@class='gmFkV']").text
 
-    def find_followings(self, driver, buttons): # this functions finds the accounts who are followed by us
-
-        # who do we follow
+    def find_followers(self, driver, buttons): # this functions finds the accounts who are following us
+        # who follows us
         self.following_button = [button for button in buttons if 'following' in button.get_attribute('href')]
         self.following_button[0].click()
         time.sleep(2)
-        self.following_window = driver.find_element_by_xpath("//div[@role='dialog']//a")
-        self.following_number = driver.find_element_by_xpath( "//*[@id='react-root']/section/main/div/header/section/ul/li[3]/a/span").text
-        counter = 0
-        while counter < int( self.following_number) / 5:  # scrolls 5 account each time approximately, if in your browser it differs, change the value with the passed account per scrolling
-            self.following_window.send_keys(Keys.PAGE_DOWN)
-            counter = counter + 1
-            time.sleep(0.2)
-        self.following_accounts = driver.find_elements_by_xpath("//a[@class='FPmhX notranslate zsYNt ']")
-        self.following_accounts = [account.get_attribute('title') for account in self.following_accounts]  # the array of the accounts who we follow
-        driver.find_element_by_xpath("//span[@class='glyphsSpriteX__outline__24__grey_9 u-__7']").click() #closes the following window
-
-
-
-    def find_followers(self, driver, buttons): # this functions finds the accounts who are following us
-        # who follows us
-        follower_button = [button for button in buttons if 'followers' in button.get_attribute('href')]
-        follower_button[0].click()
-        time.sleep(2)
         follower_window = driver.find_element_by_xpath("//div[@role='dialog']//a")
-        follower_number = driver.find_element_by_xpath("//*[@id='react-root']/section/main/div/header/section/ul/li[2]/a/span").text
+        self.follower_number = driver.find_element_by_xpath( "//*[@id='react-root']/section/main/div/header/section/ul/li[3]/a/span").text
         counter = 0
-        while counter < int( follower_number) / 5:  # scrolls 5 account each time approximately, if in your browser it differs, change the value with the passed account per scrolling
-            follower_window.send_keys(Keys.PAGE_DOWN)
-            counter = counter + 1
-            time.sleep(0.2)
-        self.follower_accounts = driver.find_elements_by_xpath("//a[@class='FPmhX notranslate zsYNt ']")
-        self.follower_accounts = [account.get_attribute('title') for account in  self.follower_accounts]  # the array of the accounts who follows us
-        driver.find_element_by_xpath("//span[@class='glyphsSpriteX__outline__24__grey_9 u-__7']").click() #closes the follower window
+        while counter < int( self.follower_number) / 10:
+            counter+=1
+            scroll_number = 1000 * counter
+            driver.execute_script(f"document.getElementsByClassName('isgrP')[0].scroll(0, {scroll_number})")
+            time.sleep(1)
+        follower_accounts = driver.find_elements_by_class_name("_0imsa")
+        self.follower_accounts = []
+        for account in follower_accounts:
+            self.follower_accounts.append(account.get_attribute('title'))
 
-    def compare_following_and_followers(self, followers, followings): # this function compare the list of followers and followings, and create a blacklist which will include the list of users who we want to unfollow.
-        followers = set(self.follower_accounts)
-        followings = set(self.following_accounts)
-        targetusers = followings - followers
-        for acc in targetusers:
-            self._blacklist.append(acc)
-            
-    def find_target_users(self): #method to find users who we follow but they don't follow us back
+    def find_target_users(self): # method to find users who we follow but they don't follow us back
         driver = self.driver
         driver.get("https://www.instagram.com/" + self._username + "/")
         time.sleep(2)
 
         buttons = driver.find_elements_by_xpath("//a[@class='-nal3 ']")
 
-        # who do we follow
-        self.find_followings(driver, buttons)
-
-        time.sleep(2)
-
         # who follows us
         self.find_followers(driver, buttons)
 
         time.sleep(2)
-        self.compare_following_and_followers(self.follower_accounts,self.following_accounts)
+        self._unfollow_list = self.follower_accounts
         time.sleep(2)
 
-
-
-
-    def unfollow_target_users(self, unfollowing_speed):
+    def unfollow_target_users(self):
         driver = self.driver
-        self.following_button[0].click()
         self.following_window = driver.find_element_by_xpath("//div[@role='dialog']//a")
-        self.following_number = driver.find_element_by_xpath(
-            "//*[@id='react-root']/section/main/div/header/section/ul/li[3]/a/span").text
-        counter = 0
-        while counter < int( self.following_number) / 5:  # scrolls 5 account each time approximately, if in your browser it differs, change the value with the passed account per scrolling
-            self.following_window.send_keys(Keys.PAGE_DOWN)
-            counter = counter + 1
-            time.sleep(0.2)
-        self.unfollow_buttons = driver.find_elements_by_xpath("//button[@class='oF4XW sqdOP  L3NKy   _8A5w5   ']")
-        for account in self._blacklist:
-            self.unfollow_buttons[self.following_accounts.index(str(account))].click()
+        self.follower_number = driver.find_element_by_xpath("//*[@id='react-root']/section/main/div/header/section/ul/li[2]/a/span").text
+        self.unfollow_buttons = driver.find_elements_by_class_name("_8A5w5")
+        for account in self._unfollow_list:
+            self.unfollow_buttons[self._unfollow_list.index(str(account))].click()
             time.sleep(0.5)
             driver.find_element_by_xpath("//button[@class='aOOlW -Cab_   ']").click()
-            time.sleep(unfollowing_speed)
-        self.following_window.find_element_by_xpath("//span[@class='glyphsSpriteX__outline__24__grey_9 u-__7']").click()
+            time.sleep(self.unfollowing_speed)
+        close_button = driver.find_elements_by_id("react-root")[0]
+        action = ActionChains(driver)
+        action.move_to_element_with_offset(close_button, 5, 5).click().perform() # closes the followers window
--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from InstaUnfollower import InstagramUnfollower
 
 def main():
 
-    username = "ozanyurtsever95@gmail.com"
-    password = "OZANMOZAN"
+    username = "username"
+    password = "password"
 
-    unfollower = InstagramUnfollower(username,password, 3) #just increase the number to unfollow slower, and decrease to unfollow faster. ( Making too fast may possible shows you as a bot to Instagram )
+    unfollower = InstagramUnfollower(username, password, 3) #just increase the number to unfollow slower, and decrease to unfollow faster. ( Making too fast may possible shows you as a bot to Instagram )
     unfollower.login() #logins your account
     unfollower.find_username() #finds the username of your account
     unfollower.find_target_users() #finds the target users who we want to unfollow
@@ -13,4 +13,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
